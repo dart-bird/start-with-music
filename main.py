30370d25f2dcc5069994a5d4e125ed0b8faa81c7
krumaska <okokrt@gmail.com>
2020-04-20 11:58:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import logging
 import random
 import time
 import os
+
 # import pyperclip
 from bs4 import BeautifulSoup
-# from playsound import playsound
 from selenium import webdriver
-# from selenium.common.exceptions import (NoSuchElementException,
-#                                         WebDriverException)
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 
 # WebDriver Path
