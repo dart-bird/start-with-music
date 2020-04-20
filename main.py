bcfa379515f44754d839bf68489671f245ef6756
krumaska <okokrt@gmail.com>
2020-04-20 09:42:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import random
 import time
-
+import os
 import pyperclip
 from bs4 import BeautifulSoup
 from playsound import playsound
@@ -11,13 +11,27 @@ from selenium.common.exceptions import (NoSuchElementException,
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
 
+webdriver_path = "./chromedriver.exe"
 initLink = "https://www.youtube.com/feed/trending?bp=4gIuCggvbS8wNHJsZhIiUExGZ3F1TG5MNTlhazRyWjBFRjlCenhOSVFBUG9ubGRYeA%3D%3D"
-music_driver = webdriver.Chrome("./chromedriver.exe")
+
+music_driver = webdriver.Chrome(webdriver_path)
+
+#Extensions
+youtubeAdBlock = ".\\chromeExtensions\\youtubeAdBlock\\youtubeAdBlock_extension.crx"
+youtubeNoneStop = ".\\chromeExtensions\\youtubeNoneStop\\youtubeNoneStop_extension.crx"
+
+def SettingExtension(webdriver_path):
+    os.environ["webdriver.chrome.driver"] = webdriver_path
+    chrome_options = Options()
+    chrome_options.add_extension(youtubeAdBlock)
+    chrome_options.add_extension(youtubeNoneStop)
 
 def StartWithChrome():
     music_driver.get(initLink)
-    youtubeList = music_driver.find_element_by_id('video-title')
-    print(youtubeList)
+    youtubeList = music_driver.find_elements_by_id('video-title')
+    youtubeList[random.randint(0,len(youtubeList)-1)].click()
 
+SettingExtension(webdriver_path=webdriver_path)
 StartWithChrome()
