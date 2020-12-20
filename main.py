c89a39813794423bbe60d1dcc96a9968f8a18759
Jinwoong <okokrt@gmail.com>
2020-12-20 18:01:32 +0900

    :hammer: Refactoring code main.py

--- main.py
+++ main.py
@@ -30,22 +30,30 @@ print("Total Search Title ~ ", searchTitle)
 music_driver = webdriver.Chrome(webdriver_path, options=chrome_options)
 
 
-# Func
-class StartWithChrome():
-    def youtube_playList(self):
-        music_driver.get(
-            'https://www.youtube.com/feed/trending?bp=4gIuCggvbS8wNHJsZhIiUExGZ3F1TG5MNTlhazRyWjBFRjlCenhOSVFBUG9ubGRYeA%3D%3D'
-        )
-        youtubeList = music_driver.find_elements_by_id('video-title')
-        youtubeList[random.randint(0, len(youtubeList) - 1)].click()
+class Music_player:
+    music_driver = None
 
-    def youtube_searchList(self):
-        currentPlayTitle = searchTitle[random.randint(0, len(searchTitle) - 1)]
+    def __init__(self, input_driver):
+        super().__init__()
+        self.music_driver = input_driver
+
+    def _youtube_select_random_search_query(self, search_query):
+        currentPlayTitle = search_query[random.randint(0, len(searchTitle) - 1)]
         print("Today Song Picked PlayList ~ %s" % currentPlayTitle)
-        initLink = "https://www.youtube.com/results?search_query=" + currentPlayTitle
-        music_driver.get(initLink)
-        youtubeList = music_driver.find_elements_by_id('video-title')
+        return "https://www.youtube.com/results?search_query=" + currentPlayTitle
+
+    def youtube_playList(self, playlist_link):
+        # EX) playlist_link = "https://www.youtube.com/feed/trending?bp=4gIuCggvbS8wNHJsZhIiUExGZ3F1TG5MNTlhazRyWjBFRjlCenhOSVFBUG9ubGRYeA%3D%3D"
+        self.music_driver.get(playlist_link)
+        youtubeList = self.music_driver.find_elements_by_id("video-title")
+        youtubeList[random.randint(0, len(youtubeList) - 1)].click()
+
+    def youtube_play(self, search_query):
+        self.music_driver.get(self._youtube_select_random_search_query(search_query))
+        youtubeList = self.music_driver.find_elements_by_id("video-title")
         youtubeList[random.randint(0, len(youtubeList) - 1)].click()
 
 
-StartWithChrome.youtube_searchList(self=None)
+if __name__ == "__main__":
+    music_player = Music_player(input_driver=music_driver)
+    music_player.youtube_play(search_query=searchTitle)
